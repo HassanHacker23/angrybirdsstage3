878d35252f1e275ca683bdd31a1a822d3875f213
@HassanHacker23 <Nabhan.h23@gmail.com>
2021-02-14 22:01:34 -0500

    added a box

--- sketch.js
+++ sketch.js
@@ -23,6 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    box6 = new Box(600,320,70,70);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -58,6 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    box6.display();
 
     bird.display();
     platform.display();
